0784f36259304e7c289a324a02e545e3bd4cbc9f
EricKimchi <EricKimchi@users.noreply.github.com>
2020-09-02 20:27:33 -0700

    update dots
    create Dot class
    create color change function

--- game.py
+++ game.py
@@ -1,7 +1,5 @@
 import os, pygame
 
-# TODO: test color swapping
-
 
 pygame.init()
 
@@ -13,8 +11,37 @@ pygame.display.set_caption("Mastermind")
 icon = pygame.image.load('m.png')
 pygame.display.set_icon(icon)
 
-# dots
-dot1 = pygame.image.load('red.png')
+
+# Dot class
+class Dot():
+    def __init__(self):
+        self.dot = pygame.image.load('red.png')
+        self.id = 0
+        self.colorid = 0
+
+# create dots
+dot1 = Dot()
+
+def changeColor(dot):
+    red = pygame.image.load('red.png')
+    orange = pygame.image.load('orange.png')
+    yellow = pygame.image.load('yellow.png')
+    green = pygame.image.load('green.png')
+    blue = pygame.image.load('blue.png')
+    purple = pygame.image.load('purple.png')
+
+    colorCode = [red, orange, yellow, green, blue, purple]
+
+    for i in range (5):
+        if dot.colorid == 5:
+            dot.dot = red
+            dot.colorid = 0
+            print("here")
+            break
+        elif dot.colorid == i:
+            dot.dot = colorCode[i + 1]
+            dot.colorid = i + 1
+            break
 
 
 # main game loop
@@ -25,7 +52,7 @@ while active:
     screen.fill((180, 180, 180))
 
     # draw dot
-    screen.blit(dot1, (300, 400))
+    screen.blit(dot1.dot, (300, 400))
     pygame.display.update()
 
 
@@ -40,4 +67,5 @@ while active:
         if event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 1:
                 if 300 < pygame.mouse.get_pos()[0] < 332 and 400 < pygame.mouse.get_pos()[1] < 432:
-                    print("clicked me")
+                    changeColor(dot1)
+
