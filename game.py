6f870e5956f3744d62009daa14fc3c9bc94850a7
EricKimchi <EricKimchi@users.noreply.github.com>
2020-09-02 19:28:24 -0700

    switch from turtle to pygame

--- game.py
+++ game.py
@@ -1,64 +1,43 @@
-# Mastermind game
+import os, pygame
 
-import turtle, os, random
+# TODO: test color swapping
 
-# dots cycle through six colors
-def changeColor(dot):
-    colorcode = ["red","orange","yellow","green","blue","purple"]
-    if dot.colorid == 5:
-        dot.circle.fillcolor("red")
-        dot.colorid = 0
-    else:
-        dot.circle.fillcolor(colorcode[dot.colorid + 1])
-        dot.colorid += 1
 
-# quad class to track patterns
-class Quad:
-    def __init__(self):
-        self.level = 0
-        self.pattern = ['gray','gray','gray','gray']
-        self.hint = ['gray','gray','gray','gray']
+pygame.init()
 
-# dot class
-class Dot:
-    def __init__(self):
-        self.circle = turtle.Turtle()
-        self.circle.color("gray","red")
-        self.circle.shape("circle")
-        self.id = 0
-        self.colorid = 0
-    
+# create the screen
+screen = pygame.display.set_mode((600,600))
 
-# set up a screen
-screen = turtle.Screen()
-screen.bgcolor("black")
-screen.title("MASTERMIND")
+# title and icon
+pygame.display.set_caption("Mastermind")
+icon = pygame.image.load('m.png')
+pygame.display.set_icon(icon)
 
-# draw a border
-border_pen = turtle.Turtle()
-border_pen.speed(0)
-border_pen.color("gray")
-border_pen.penup()
-border_pen.setposition(-300, -300)
-border_pen.pendown()
-border_pen.pensize(3)
-for side in range(4):
-    border_pen.fd(600)
-    border_pen.lt(90)
-border_pen.hideturtle()
+# dots
+dot1 = pygame.image.load('red.png')
 
-# create a circle
-dot1 = Dot()
-dot1.circle.penup()
-dot1.circle.speed(0)
-dot1.circle.setposition(200,200)
 
-# make 8 Quads to track the guesses
-quads = []
-for i in range(8):
-    quads.append(Quad())
-    quads[i].level = i
+# main game loop
+active = True
+while active:
 
-#Main game loop
-while True:
-    dot1.circle.onclick(lambda x, y: changeColor(dot1))
+    # background color to light gray
+    screen.fill((180, 180, 180))
+
+    # draw dot
+    screen.blit(dot1, (300, 400))
+    pygame.display.update()
+
+
+
+
+
+
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            active = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1:
+                if 300 < pygame.mouse.get_pos()[0] < 332 and 400 < pygame.mouse.get_pos()[1] < 432:
+                    print("clicked me")
